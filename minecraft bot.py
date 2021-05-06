31a4066669b43a387584120c07a1f5f5b3de3549
AnxDev <82952385+AnxDev@users.noreply.github.com>
2021-05-06 21:30:12 +0200

    Add files via upload

---
+++ minecraft bot.py
@@ -0,0 +1,36 @@
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import numpy as np
+import win32api, win32con
+
+time.sleep(5)
+
+# definisco il click con il win32 api
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0) # clicca
+    time.sleep(0.1) # attende il click
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0) # rilascia
+def click2(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0) # clicca
+    time.sleep(5) # attende il click
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0) # rilascia
+click(962, 393)
+time.sleep(2)
+click(1110, 411)
+
+time.sleep(2)
+click(921, 167)
+time.sleep(0.5)
+click(922, 167)
+time.sleep(3.2)
+# seed: -3635737710501644033
+pyautogui.write("-3635737710501644033")
+time.sleep(4)
+click(794, 1004)
+time.sleep(15)
+click2(959, 533)
